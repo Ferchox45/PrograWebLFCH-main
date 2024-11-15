424967b6f100ade4896b0dd5e35cec294cac60ff
Ferchox45 <fer.con0956@gmail.com>
2024-11-14 23:05:57 -0600

    Servicios de usuario

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'https://api.escuelajs.co/api/v1/users';
+
+  constructor(private http: HttpClient) {}
+
+  // Método para obtener la lista de usuarios
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+}
